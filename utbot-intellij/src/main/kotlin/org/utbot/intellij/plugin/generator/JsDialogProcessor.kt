e74001087909f505f405426dcda00ba3a14dbd6d
Sergey Loktev <loktevsergay.2003@gmail.com>
2022-08-12 10:10:33 +0300

    Exports management fix (finished)

--- utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/generator/JsDialogProcessor.kt
+++ utbot-intellij/src/main/kotlin/org/utbot/intellij/plugin/generator/JsDialogProcessor.kt
@@ -259,7 +259,7 @@ object JsDialogProcessor {
             fileText.contains("module.exports = {$exportLine}") -> {
             }
             fileText.contains(startComment) && !fileText.contains("module.exports = {$exportLine}") -> {
-                val regex = Regex("$startComment\n.*\n$endComment")
+                val regex = Regex("\n$startComment\n(.*)\n$endComment")
                 regex.find(fileText)?.groups?.get(1)?.value?.let {
                     val swappedText = fileText.replace(it, "module.exports = {$exportLine}")
                     file.writeText(swappedText)
