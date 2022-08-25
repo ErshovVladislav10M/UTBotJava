588cb1439e4cfd7da06a6657da108682a01800d6
Sergey Loktev <loktevsergay.2003@gmail.com>
2022-08-25 12:38:42 +0300

    run_js CLI command made argument as required option

--- utbot-cli/src/main/kotlin/org/utbot/cli/JsRunTestsCommand.kt
+++ utbot-cli/src/main/kotlin/org/utbot/cli/JsRunTestsCommand.kt
@@ -1,6 +1,7 @@
 package org.utbot.cli
 
 import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.check
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
 import com.github.ajalt.clikt.parameters.options.required
@@ -18,6 +19,13 @@ class JsRunTestsCommand : CliktCommand(name = "run_js", help = "Runs tests for t
         help = "Specifies a file or directory with tests"
     ).required()
 
+    private val output by option(
+        "-o", "--output",
+        help = "Specifies an output .txt file for test framework result"
+    ).check("Must end with .txt suffix") {
+        it.endsWith(".txt")
+    }
+
     private val testFramework by option("--test-framework", "-t", help = "Test framework to be used")
         .choice("mocha")
         .default("mocha")
@@ -28,7 +36,7 @@ class JsRunTestsCommand : CliktCommand(name = "run_js", help = "Runs tests for t
             fileWithTests.substringBeforeLast(File.separator) else fileWithTests
         when (testFramework) {
             "mocha" -> {
-                val (text, error) = JsCmdExec.runCommand(
+                val (textReader, error) = JsCmdExec.runCommand(
                     "mocha $fileWithTests",
                     dir
                 )
@@ -36,7 +44,12 @@ class JsRunTestsCommand : CliktCommand(name = "run_js", help = "Runs tests for t
                 if (errorText.isNotEmpty()) {
                     logger.error { "An error has occurred while running tests for $fileWithTests : $errorText" }
                 } else {
-                    logger.info { text.readText() }
+                    val text = textReader.readText()
+                    output?.let {
+                        val file = File(it)
+                        file.createNewFile()
+                        file.writeText(text)
+                    } ?: logger.info { text }
                 }
             }
         }
